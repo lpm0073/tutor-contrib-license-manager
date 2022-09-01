6b6109bda9450021b7a02ff99365f480a668f2cc
lpm0073 <lpm0073@gmail.com>
2022-09-01 12:00:53 -0500

    plugin is now named license-manager

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     install_requires=["tutor"],
     entry_points={
         "tutor.plugin.v1": [
-            "license_manager = license_manager.plugin"
+            "license-manager = license_manager.plugin"
         ]
     },
     classifiers=[
