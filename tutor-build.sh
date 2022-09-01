6bf2cd8c578c542a6b17ab4aac06956c7f1b4f2c
lpm0073 <lpm0073@gmail.com>
2022-09-01 18:38:11 -0500

    refactor assignment of k8s secrets variables

--- tutor-build.sh
+++ tutor-build.sh
@@ -18,7 +18,6 @@ AWS_ECR_REGISTRY="${AWS_ACCOUNT_NUMBER}.dkr.ecr.${AWS_REGION}.amazonaws.com"
 # this is stored locally in /home/ubuntu/.docker/config.json and lasts for 12 hours.
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ECR_REGISTRY
 
-
 LMS_HOST="web.stepwise.ai"
 HOST="subscriptions.${LMS_HOST}"
 
@@ -31,32 +30,25 @@ pip uninstall tutor-contrib-license-manager -y
 pip install git+${PLUGIN_REPO}
 tutor plugins enable ${PLUGIN_NAME}
 
-#ksecret.sh mysql-license-manager stepwisemath-global-prod
-
 # Plugin configuration
-# ----------------------------------------------
-LICENSE_MANAGER_MYSQL_DATABASE=webstepwisemathai_prod_lm
-LICENSE_MANAGER_MYSQL_PASSWORD=FFfH6Qq4ww_9xQ6j
-LICENSE_MANAGER_MYSQL_USERNAME=webstepwisemathai_prod_lm
-MYSQL_HOST=stepwisemath-global-live.cqkw93ffci2g.us-east-2.rds.amazonaws.com
-MYSQL_PORT=3306
-
-LICENSE_MANAGER_OAUTH2_KEY_DEV='dev-key'
-LICENSE_MANAGER_OAUTH2_SECRET='dev-secret'
-LICENSE_MANAGER_OAUTH2_KEY='prod-key'
-LICENSE_MANAGER_OAUTH2_SECRET='prod-secret'
+# -----------------------------------------------------------------------------
+# retrieve mysql-license-manager secret from k8s, decode,
+# parse and push variables to the terminal. examples:
+#
+#   TUTOR_LICENSE_MANAGER_MYSQL_DATABASE=webstepwisemathai_prod_lm
+#   TUTOR_LICENSE_MANAGER_MYSQL_PASSWORD=super-duper-password
+#   TUTOR_LICENSE_MANAGER_MYSQL_USERNAME=webstepwisemathai_prod_lm
+#   TUTOR_MYSQL_HOST=stepwisemath-global-live.cqkw93ffci2g.us-east-2.rds.amazonaws.com
+#   TUTOR_MYSQL_PORT=3306
+# -----------------------------------------------------------------------------
+$(kubectl get secret mysql-license-manager -n stepwisemath-global-prod  -o json | jq  '.data | map_values(@base64d)' |   jq -r 'keys[] as $k | "export TUTOR_\($k|ascii_upcase)=\(.[$k])"')
 
 tutor config save --set "LICENSE_MANAGER_LMS_HOST=${LMS_HOST}" \
                   --set "LICENSE_MANAGER_HOST=${HOST}" \
-		          --set "LICENSE_MANAGER_MYSQL_DATABASE=${LICENSE_MANAGER_MYSQL_DATABASE}" \
-                  --set "LICENSE_MANAGER_MYSQL_PASSWORD=${LICENSE_MANAGER_MYSQL_PASSWORD}" \
-                  --set "LICENSE_MANAGER_MYSQL_USERNAME=${LICENSE_MANAGER_MYSQL_USERNAME}" \
-                  --set "MYSQL_HOST=${MYSQL_HOST}" \
-                  --set "MYSQL_PORT=${MYSQL_PORT}" \
-                  --set "LICENSE_MANAGER_OAUTH2_KEY_DEV=${LICENSE_MANAGER_OAUTH2_KEY_DEV}" \
-                  --set "LICENSE_MANAGER_OAUTH2_SECRET=${LICENSE_MANAGER_OAUTH2_SECRET}" \
-                  --set "LICENSE_MANAGER_OAUTH2_KEY=${LICENSE_MANAGER_OAUTH2_KEY}" \
-                  --set "LICENSE_MANAGER_OAUTH2_SECRET=${LICENSE_MANAGER_OAUTH2_SECRET}" \
+                  --set "LICENSE_MANAGER_OAUTH2_KEY_DEV='dev-key'" \
+                  --set "LICENSE_MANAGER_OAUTH2_SECRET_DEV='dev-secret'" \
+                  --set "LICENSE_MANAGER_OAUTH2_KEY='prod-key'" \
+                  --set "LICENSE_MANAGER_OAUTH2_SECRET='prod-secret'" \
 
 # Docker build / AWS ECR upload
 # ----------------------------------------------
@@ -71,7 +63,7 @@ DOCKER_IMAGE_LATEST="${AWS_ECR_REPOSITORY_URL}:latest"
 tutor config save --set "LICENSE_MANAGER_DOCKER_IMAGE=${DOCKER_IMAGE}"
 
 tutor images build ${PLUGIN_NAME}
-tutor images push ${PLUGIN_NAME}
+tutor images push ${PLUGIN_NAME}        # FIX NOTE: Error: Image 'license_manager' could not be found
 
 docker tag ${DOCKER_IMAGE} ${DOCKER_IMAGE_LATEST}
 docker push ${DOCKER_IMAGE_LATEST}
